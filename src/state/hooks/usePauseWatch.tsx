fef31894b64d22602310bbb5caba3d7f4c7800c2
Jefté Lima <jefte_lima@hotmail.com>
2023-12-17 11:50:37 -0300

    Changed remaining time calculation

--- src/state/hooks/usePauseWatch.tsx
+++ src/state/hooks/usePauseWatch.tsx
@@ -1,9 +1,9 @@
 import { useRecoilState } from "recoil";
 import { IWatch } from "@interfaces/IWatch";
-import { tasksState, watchState } from "@state/atom";
 import { ITask } from "@interfaces/ITask";
+import { tasksState, watchState } from "@state/atom";
+import { formatSecondsToString, formatStringToSeconds } from "@common/utils/timeFormatter";
 import useGetSelectedTask from "./useGetSelectedTask";
-import { formatSecondsToString } from "@common/utils/timeFormatter";
 
 export default function usePause() {
   const [watch, setWatch] = useRecoilState<IWatch>(watchState);
@@ -14,11 +14,20 @@ export default function usePause() {
   return () => {
     setWatch({ ...watch, run: false });
 
+    let remainingTimeString = formatSecondsToString(watch.value);
+
+    if(selectedTask?.remainingTime) {
+      const remainingTimeInSeconds = formatStringToSeconds(selectedTask?.remainingTime);
+      const timePassedInSeconds = remainingTimeInSeconds - watch.value;
+      const remainingValueInSeconds = remainingTimeInSeconds - timePassedInSeconds;
+      remainingTimeString = formatSecondsToString(remainingValueInSeconds);
+    }
+
     setTasks(tasks.map(task => {
       if(task.id === selectedTask?.id) {
         return {
           ...task,
-          remainingTime: formatSecondsToString(watch.value)
+          remainingTime: remainingTimeString
         }
       }
       return task
