7d42251d6f97dd6aa2804b123be44dc42b5eb2da
Jefté Lima <jefte_lima@hotmail.com>
2024-03-02 06:29:25 -0300

    refactor(useStopWatch): enhance type safety and improve function modularity
    - Introduce explicit type `ITask | null` for `selectedTask` to ensure type safety
    - Refactor logic into `updateWatchState` and `updateTasksState` functions for clarity and maintainability

--- src/state/hooks/useStopWatch.tsx
+++ src/state/hooks/useStopWatch.tsx
@@ -1,38 +1,48 @@
-import { useRecoilState } from "recoil";
-import { IWatch } from "@interfaces/IWatch";
-import { ITask } from "@interfaces/ITask";
-import { tasksState, watchState } from "@state/atom";
-import { formatSecondsToString, formatStringToSeconds } from "@common/utils/timeFormatter";
-import useGetSelectedTask from "./useGetSelectedTask";
+import { useRecoilState } from 'recoil';
+import { tasksState, watchState } from '@state/atom';
+import { IWatch } from '@interfaces/IWatch';
+import { ITask } from '@interfaces/ITask';
+import { formatSecondsToString, formatStringToSeconds } from '@common/utils/timeFormatter';
+import useGetSelectedTask from './useGetSelectedTask';
 
 export default function useStopWatch() {
   const [watch, setWatch] = useRecoilState<IWatch>(watchState);
   const [tasks, setTasks] = useRecoilState<ITask[]>(tasksState);
 
-  const selectedTask = useGetSelectedTask();
+  const selectedTask: ITask | null = useGetSelectedTask();
+
+  function updateWatchState(timePassedInSeconds: number) {
+    setWatch({ ...watch, initialValue: watch.initialValue - timePassedInSeconds, run: false });
+  }
+
+  function updateTasksState(remainingTimeString: string) {
+    return tasks.map((task) => {
+      if (task.id === selectedTask?.id) {
+        return {
+          ...task,
+          remainingTime: remainingTimeString,
+        };
+      }
+      return task;
+    });
+  }
 
   return () => {
-    if(selectedTask?.completed) return
+    if (selectedTask?.completed) {
+      return;
+    }
 
     let remainingTimeString = formatSecondsToString(watch.value);
     const timePassedInSeconds = watch.initialValue - watch.value;
 
-    if(selectedTask) {
+    if (selectedTask) {
       const remainingTimeInSeconds = formatStringToSeconds(selectedTask.remainingTime);
       const remainingValueInSeconds = remainingTimeInSeconds - timePassedInSeconds;
       remainingTimeString = formatSecondsToString(remainingValueInSeconds);
 
-      setTasks(tasks.map(task => {
-        if(task.id === selectedTask.id) {
-          return {
-            ...task,
-            remainingTime: remainingTimeString
-          }
-        }
-        return task
-      }));
+      setTasks(updateTasksState(remainingTimeString));
     }
 
-    setWatch({ ...watch, initialValue: watch.initialValue - timePassedInSeconds,  run: false });
+    updateWatchState(timePassedInSeconds);
   };
 }
